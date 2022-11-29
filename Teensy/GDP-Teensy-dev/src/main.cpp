87b25bf38b340e8b1d01e8850899e11eeecb92f2
joehughes01 <mc20j2h@leeds.ac.uk>
2022-11-29 13:49:20 +0000

    more compiler fixing

--- Teensy/GDP-Teensy-dev/src/main.cpp
+++ Teensy/GDP-Teensy-dev/src/main.cpp
@@ -1,4 +1,15 @@
 #include <Arduino.h>
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioInputAnalog         adc1;           //xy=101,211
+AudioOutputAnalog        dac1;           //xy=307,222
+AudioConnection          patchCord1(adc1, dac1);
+// GUItool: end automatically generated code
 
 void setup() {
   
